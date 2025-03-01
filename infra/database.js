7941212553ec09ad06ede6b6142112b6447cd215
Caique Steibel <106418680+caiqueSteibel@users.noreply.github.com>
2025-03-01 08:08:47 +0000

    add `jsconfig,json` file

--- infra/database.js
+++ infra/database.js
@@ -9,10 +9,8 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD,
   });
   await client.connect();
-  await client.query(queryObject);
   const result = await client.query(queryObject);
   await client.end();
-
   return result;
 }
 
